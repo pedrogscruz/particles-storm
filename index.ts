d1f7b03c3ccbf9388add974f305e8cd44396e513
pedrogscruz <pedro.gscruz@hotmail.com>
2024-12-17 14:25:58 -0300

    Add ParticlesPolygon to index export

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { default as ParticlesStormBackground } from "./src/components/particles-storm-background";
 export { default as ParticlesStorm } from "./src/components/particles-storm";
+export { default as ParticlesPolygon } from "./src/components/particles-polygon";
